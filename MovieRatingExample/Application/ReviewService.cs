525ae4e3417d96448e6fc29c6af4b13c47d9208c
Onii-chan <failer007@live.dk>
2022-09-26 14:19:09 +0200

    Implemented 2-7
    * 7 test needs tweaking.

--- MovieRatingExample/Application/ReviewService.cs
+++ MovieRatingExample/Application/ReviewService.cs
@@ -39,7 +39,17 @@ namespace MovieRatingExample.Application
 
         public double GetAverageRateOfMovie(int movie)
         {
-            throw new NotImplementedException();
+            int count = 0;
+            int score = 0;
+            foreach (BEReview review in Repository.GetAll())
+            {
+                if (review.Movie == movie)
+                {
+                    count++;
+                    score += review.Grade;
+                }
+            }
+            return score > 0 ? score / count : 0;
         }
 
         public List<int> GetMostProductiveReviewers()
@@ -49,22 +59,76 @@ namespace MovieRatingExample.Application
 
         public List<int> GetMoviesWithHighestNumberOfTopRates()
         {
-            throw new NotImplementedException();
+            var ids = new Dictionary<int, int>();
+
+            foreach (BEReview review in Repository.GetAll())
+            {
+                if (review.Grade == 5)
+                {
+                    if (!ids.ContainsKey(review.Movie))
+                        ids.Add(review.Movie, 1);
+                    else
+                    {
+                        ids[review.Movie]++;
+                    }
+                }
+            }
+
+            //var collection = new Dictionary<int, int>();
+            int highestNumber = 0;
+            foreach (var entry in ids)
+            {
+                var foundAmount = entry.Value;
+
+                if (foundAmount > highestNumber)
+                    highestNumber = foundAmount;
+
+            }
+
+            var result = new List<int>();
+            foreach (var entry in ids)
+            {
+                var movieId = entry.Key;
+                var foundAmount = entry.Value;
+
+                if (foundAmount == highestNumber)
+                    result.Add(movieId);
+            }
+
+            return result;
         }
 
         public int GetNumberOfRates(int movie, int rate)
         {
-            throw new NotImplementedException();
+            int count = 0;
+            foreach (BEReview review in Repository.GetAll())
+            {
+                if (review.Movie == movie && review.Grade == rate)
+                    count++;
+            }
+            return count;
         }
 
         public int GetNumberOfRatesByReviewer(int reviewer, int rate)
         {
-            throw new NotImplementedException();
+            int count = 0;
+            foreach (BEReview review in Repository.GetAll())
+            {
+                if (review.Reviewer == reviewer && review.Grade == rate)
+                    count++;
+            }
+            return count;
         }
 
         public int GetNumberOfReviews(int movie)
         {
-            throw new NotImplementedException();
+            int count = 0;
+            foreach (BEReview review in Repository.GetAll())
+            {
+                if (review.Movie == movie)
+                    count++;
+            }
+            return count;
         }
 
         public int GetNumberOfReviewsFromReviewer(int reviewer)
--- XUnitTestProject/ReviewServiceTest.cs
+++ XUnitTestProject/ReviewServiceTest.cs
@@ -89,5 +89,139 @@ namespace XUnitTestProject
             Assert.Equal(expectedResult, result);
             mockRepository.Verify(r => r.GetAll(), Times.Once);
         }
+
+        [Theory]
+        [InlineData(1, 4, 2)]
+        [InlineData(1, 5, 1)]
+        [InlineData(1, 3, 0)]
+        public void GetNumberOfRatesByReviewer(int reviewer, int rate, int expectedResult)
+        {
+            // Arrange
+            BEReview[] fakeRepo = new BEReview[]
+            {
+                new BEReview() {Reviewer = 1, Movie = 1, Grade=4, ReviewDate = new DateTime()},
+                new BEReview() {Reviewer = 1, Movie = 1, Grade=4, ReviewDate = new DateTime()},
+                new BEReview() {Reviewer = 1, Movie = 1, Grade=5, ReviewDate = new DateTime()},
+            };
+
+            Mock<IReviewRepository> mockRepository = new Mock<IReviewRepository>();
+            mockRepository.Setup(r => r.GetAll()).Returns(fakeRepo);
+
+            IReviewService service = new ReviewService(mockRepository.Object);
+
+            // Act
+            int result = service.GetNumberOfRatesByReviewer(reviewer, rate);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+            mockRepository.Verify(r => r.GetAll(), Times.Once);
+        }
+
+        [Theory]
+        [InlineData(1, 3)]
+        [InlineData(2, 1)]
+        [InlineData(3, 0)]
+        public void GetNumberOfReviews(int movie, int expectedResult)
+        {
+            // Arrange
+            BEReview[] fakeRepo = new BEReview[]
+            {
+                new BEReview() {Reviewer = 1, Movie = 1, Grade=4, ReviewDate = new DateTime()},
+                new BEReview() {Reviewer = 1, Movie = 1, Grade=4, ReviewDate = new DateTime()},
+                new BEReview() {Reviewer = 1, Movie = 1, Grade=5, ReviewDate = new DateTime()},
+                new BEReview() {Reviewer = 1, Movie = 2, Grade=5, ReviewDate = new DateTime()},
+            };
+
+            Mock<IReviewRepository> mockRepository = new Mock<IReviewRepository>();
+            mockRepository.Setup(r => r.GetAll()).Returns(fakeRepo);
+
+            IReviewService service = new ReviewService(mockRepository.Object);
+
+            // Act
+            int result = service.GetNumberOfReviews(movie);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+            mockRepository.Verify(r => r.GetAll(), Times.Once);
+        }
+
+        [Theory]
+        [InlineData(1, 3)]
+        [InlineData(2, 5)]
+        [InlineData(3, 0)]
+        public void GetAverageRateOfMovie(int movie, double expectedResult)
+        {
+            // Arrange
+            BEReview[] fakeRepo = new BEReview[]
+            {
+                new BEReview() {Reviewer = 1, Movie = 1, Grade=2, ReviewDate = new DateTime()},
+                new BEReview() {Reviewer = 1, Movie = 1, Grade=4, ReviewDate = new DateTime()},
+                new BEReview() {Reviewer = 1, Movie = 2, Grade=5, ReviewDate = new DateTime()},
+            };
+
+            Mock<IReviewRepository> mockRepository = new Mock<IReviewRepository>();
+            mockRepository.Setup(r => r.GetAll()).Returns(fakeRepo);
+
+            IReviewService service = new ReviewService(mockRepository.Object);
+
+            // Act
+            double result = service.GetAverageRateOfMovie(movie);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+            mockRepository.Verify(r => r.GetAll(), Times.Once);
+        }
+
+        [Theory]
+        [InlineData(1, 2, 2)]
+        [InlineData(2, 5, 1)]
+        [InlineData(3, 4, 0)]
+        public void GetNumberOfRates(int movie, int rate, int expectedResult)
+        {
+            // Arrange
+            BEReview[] fakeRepo = new BEReview[]
+            {
+                new BEReview() {Reviewer = 1, Movie = 1, Grade=2, ReviewDate = new DateTime()},
+                new BEReview() {Reviewer = 1, Movie = 1, Grade=2, ReviewDate = new DateTime()},
+                new BEReview() {Reviewer = 1, Movie = 2, Grade=5, ReviewDate = new DateTime()},
+            };
+
+            Mock<IReviewRepository> mockRepository = new Mock<IReviewRepository>();
+            mockRepository.Setup(r => r.GetAll()).Returns(fakeRepo);
+
+            IReviewService service = new ReviewService(mockRepository.Object);
+
+            // Act
+            int result = service.GetNumberOfRates(movie, rate);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+            mockRepository.Verify(r => r.GetAll(), Times.Once);
+        }
+
+        [Fact]
+        public void GetMoviesWithHighestNumberOfTopRates()
+        {
+            // Arrange
+            BEReview[] fakeRepo = new BEReview[]
+            {
+                new BEReview() {Reviewer = 1, Movie = 1, Grade=5, ReviewDate = new DateTime()},
+                new BEReview() {Reviewer = 1, Movie = 1, Grade=5, ReviewDate = new DateTime()},
+                new BEReview() {Reviewer = 1, Movie = 1, Grade=5, ReviewDate = new DateTime()},
+                new BEReview() {Reviewer = 1, Movie = 2, Grade=5, ReviewDate = new DateTime()},
+            };
+
+            Mock<IReviewRepository> mockRepository = new Mock<IReviewRepository>();
+            mockRepository.Setup(r => r.GetAll()).Returns(fakeRepo);
+
+            IReviewService service = new ReviewService(mockRepository.Object);
+
+            // Act
+            List<int> result = service.GetMoviesWithHighestNumberOfTopRates();
+
+            // Assert
+            Assert.Equal(1, result.Count);
+            mockRepository.Verify(r => r.GetAll(), Times.Once);
+        }
     }
 }
