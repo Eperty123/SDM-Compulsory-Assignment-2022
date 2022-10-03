a3156eed4554d877577e5482b0034590f45ae28f
Onii-chan <failer007@live.dk>
2022-10-03 14:25:50 +0200

    Added json test

---
+++ MovieRatingExample.Infrastructure.Data/Repositories/ReviewRepository.cs
@@ -0,0 +1,20 @@
+﻿using MovieRatingExample.Core.Models;
+using MovieRatingExample.Domain.IRepositories;
+
+namespace MovieRatingExample.Infrastructure.Data.Repositories
+{
+    public class ReviewRepository : IReviewRepository
+    {
+        readonly List<BEReview> reviews;
+
+        public ReviewRepository(List<BEReview> reviews)
+        {
+            this.reviews = reviews;
+        }
+
+        public BEReview[] GetAll()
+        {
+            return reviews.ToArray();
+        }
+    }
+}
--- XUnitTestProject/JsonReviewRepositoryTest.cs
+++ XUnitTestProject/JsonReviewRepositoryTest.cs
@@ -1,4 +1,6 @@
 ﻿using MovieRatingExample.Core.Models;
+using MovieRatingExample.Domain.IRepositories;
+using MovieRatingExample.Infrastructure.Data.Repositories;
 using MovieRatingExample.Infrastructure.Data.Utilities;
 using System;
 using System.Collections.Generic;
@@ -15,6 +17,17 @@ namespace XUnitTestProject
         {
             var reviews = File.ReadAllText("Resources/ratings.json").FromJson<BEReview[]>();
             Assert.NotNull(reviews);
+        }
+
+        [Theory]
+        //[InlineData("ratings.json")]
+        [InlineData("ratings_small.json")]
+        public void JsonReviewReadParseTest(string jsonFileName)
+        {
+            var reviews = File.ReadAllText($"Resources/{jsonFileName}").FromJson<BEReview[]>();
+            IReviewRepository repository = new ReviewRepository(reviews.ToList());
+
+            Assert.NotNull(repository.GetAll());
         }
     }
 }
